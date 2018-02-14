2eae2683c18c847aac4fc361c5b7e3e72c3681c7
Michal Bojanowski <michal2992@gmail.com>
2018-02-14 16:01:23 +0100

    Added elsevier_article2

--- R/elsevier_article.R
+++ R/elsevier_article.R
@@ -25,3 +25,20 @@ elsevier_article <- function(...,
                        keep_tex = keep_tex,
                        md_extensions = md_extensions)
 }
+
+
+#' @rdname elsevier_article
+#' @details
+#' `elsevier_article2` uses `bookdown::pdf_document2` so supports the bookdown syntax.
+elsevier_article2 <- function(
+  ...,
+  keep_tex = TRUE,
+  md_extensions = "-autolink_bare_uris"
+) {
+  inherit_pdf_document2(
+    ...,
+    template = find_resource("elsevier_article", "template.tex"),
+    keep_tex = keep_tex,
+    md_extensions = md_extensions
+  )
+}
